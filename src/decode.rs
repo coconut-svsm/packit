9ea76c84d670bb01558a826cdebe83bd6b47c79d
Carlos López <carlos.lopez@suse.com>
2023-06-21 17:56:41 +0200

    Initial commit
    Signed-off-by: Carlos López <carlos.lopez@suse.com>

---
+++ src/decode.rs
@@ -0,0 +1,49 @@
+use crate::{PackItFile, PackItHeader, PackItResult};
+
+/// A lazy raw PackIt archive decoder.
+#[derive(Clone, Copy, Debug)]
+pub struct PackItArchiveDecoder<'a> {
+    hdr: PackItHeader,
+    raw_data: &'a [u8],
+    current: usize,
+}
+
+impl<'a> PackItArchiveDecoder<'a> {
+    /// The archive header
+    pub fn header(&self) -> PackItHeader {
+        self.hdr
+    }
+
+    /// Load an archive from an existing blob
+    pub fn load(raw_data: &'a [u8]) -> PackItResult<Self> {
+        let hdr = PackItHeader::load(raw_data)?;
+        Ok(Self {
+            hdr,
+            raw_data,
+            current: hdr.header_size() as usize,
+        })
+    }
+}
+
+/// Iterate over packed [`PackItFile`] files.
+impl<'a> core::iter::Iterator for PackItArchiveDecoder<'a> {
+    type Item = PackItResult<PackItFile<'a>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let data = self.raw_data
+            .get(self.current..)
+            .filter(|d| !d.is_empty())?;
+
+        match PackItFile::load(data) {
+            Ok(f) => {
+                self.current += f.total_size();
+                Some(Ok(f))
+            }
+            Err(e) => {
+                // Stop iterating
+                self.current = self.raw_data.len() + 1;
+                Some(Err(e))
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,234 @@
+use clap::Parser;
+use memmap::Mmap;
+use std::fs;
+use std::io::{self, ErrorKind, Write};
+use std::path::{Component, Path, PathBuf};
+use std::process::ExitCode;
+use packit::{PackItArchiveEncoder, PackItArchiveDecoder, PackItError, PackItFile, PackItResult};
+
+#[macro_export]
+macro_rules! ioerr {
+    ($k:ident) => { io::Error::from(ErrorKind::$k) };
+    ($k:ident, $e:expr) => { io::Error::new(ErrorKind::$k, $e) };
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+	#[command(subcommand)]
+	command: Command,
+}
+
+#[derive(clap::Subcommand, Clone, Debug)]
+enum Command {
+	/// Pack a directory into an archive
+	Pack(PackParams),
+	/// Unpack an archive into a directory
+	Unpack(UnpackParams),
+	/// List files in an archive
+	List(ListParams)
+}
+
+#[derive(clap::Args, Clone, Debug)]
+struct PackParams {
+	/// Directory to pack
+	#[clap(short, long, value_parser)]
+	input: PathBuf,
+	/// Output archive
+	#[clap(short, long, value_parser)]
+	output: PathBuf,
+	/// Do not follow symlinks. If set, symlinks will be ignored.
+	#[clap(short = 's', long, value_parser)]
+	no_symlinks: bool,
+	/// Print files as they are archived
+	#[clap(short, long, value_parser)]
+	verbose: bool,
+}
+
+impl PackParams {
+	fn run(&self) -> PackItResult<()> {
+		let mut dst = fs::File::create(&self.output)?;
+		let mut ar = PackItArchiveEncoder::new(&mut dst)?;
+
+		for entry in fs::read_dir(&self.input)? {
+			let entry = entry?;
+			self.process_entry(&entry, &mut ar)?;
+		}
+
+		dst.sync_all()?;
+
+		Ok(())
+	}
+
+	fn process_entry<W: Write>(
+		&self,
+		entry: &fs::DirEntry,
+		ar: &mut PackItArchiveEncoder<W>
+	) -> PackItResult<()> {
+		let path = entry.path();
+		let file = fs::File::open(&path)?;
+		let meta = file.metadata()?;
+		let etype = meta.file_type();
+
+		if etype.is_file() || (etype.is_symlink() && !self.no_symlinks) {
+			// Create the destination path inside the archive
+			let dst_path = path.strip_prefix(&self.input)
+				.map_err(|e| ioerr!(InvalidData, e))?
+				.to_str()
+				.ok_or(PackItError::InvalidFileName)?;
+
+			if self.verbose {
+				println!("{} -> {}", path.display(), dst_path);
+			}
+
+			// Map the file and write it to the archive
+			match meta.len() {
+				0 => {
+					// Special case, a zero-length mapping will fail
+					let pfile = PackItFile::new(dst_path, &[])?;
+					ar.write_file(&pfile)?;
+				},
+				_ => {
+					let file_data = unsafe { Mmap::map(&file) }?;
+					let pfile = PackItFile::new(dst_path, &file_data)?;
+					ar.write_file(&pfile)?;
+				},
+			}
+		} else if etype.is_dir() {
+			for entry in fs::read_dir(&path)? {
+				let entry = entry?;
+				self.process_entry(&entry, ar)?;
+			}
+		}
+
+		Ok(())
+	}
+}
+
+#[derive(clap::Args, Clone, Debug)]
+struct UnpackParams {
+	/// Input archive to unpack.
+	#[clap(short, long, value_parser)]
+	input: PathBuf,
+	/// Output directory.
+	#[clap(short, long, value_parser)]
+	output: PathBuf,
+	/// Print files as they are unpacked.
+	#[clap(short, long, value_parser)]
+	verbose: bool,
+}
+
+// TODO: add test(s) for this function
+fn verify_path<P: AsRef<Path>>(path: P) -> bool {
+	// Depth zero means base directory. If we go below 0 this path
+	// points outside the base directory
+	let mut depth = 0isize;
+
+	// Get all components and remove the file name
+	let mut components = path.as_ref().components();	
+	let _ = components.next_back();
+
+	for comp in components {
+		match comp {
+			Component::CurDir |
+			Component::RootDir |
+			Component::Prefix(..) => (),
+			Component::Normal(..) => depth += 1,
+			Component::ParentDir => depth -= 1,
+		}
+	}
+
+	depth >= 0
+}
+
+impl UnpackParams {
+	fn run(&self) -> PackItResult<()> {
+		// Prepare the decodder
+		let file = fs::File::open(&self.input)?;
+		let mem = unsafe { memmap::Mmap::map(&file) }?;
+		let dec = PackItArchiveDecoder::load(&mem)?;
+
+		// Create and form the full output path
+		fs::DirBuilder::new().create(&self.output)?;
+		let outdir = self.output.canonicalize()?;
+
+		for file in dec {
+			let file = file?;
+			let file_path = Path::new(file.name());
+
+			if !verify_path(file_path) {
+				return Err(ioerr!(
+					InvalidData,
+					format!(
+						"File path points outside base directory: {}",
+						file_path.display()
+					)
+				).into());
+			}
+
+			if let Some(parent) = file_path.parent()
+				.filter(|p| !p.to_str().unwrap_or("").is_empty())
+			{
+				// Create path leading to file
+				let parent = outdir.join(parent);
+				if self.verbose {
+					println!("Directory: {}", parent.display());	
+				}
+				fs::DirBuilder::new()
+					.recursive(true)
+					.create(&parent)?;
+
+				// Check that there is no escape from the output dir
+				assert!(parent.canonicalize()
+					.unwrap()
+					.starts_with(&outdir));
+			}
+
+			let file_path = outdir.join(file_path);
+			if self.verbose {
+				println!("File: {}", file_path.display());
+			}
+
+			// Create and populate file
+			fs::File::create(&file_path)?.write_all(file.data())?;
+		}
+
+		Ok(())
+	}
+}
+
+#[derive(clap::Args, Clone, Debug)]
+struct ListParams {
+	/// Archive to list
+	#[clap(short, long, value_parser)]
+	input: PathBuf,
+}
+
+impl ListParams {
+	fn run(&self) -> PackItResult<()> {
+		let file = fs::File::open(&self.input)?;
+		let mem = unsafe { Mmap::map(&file) }?;
+		let dec = PackItArchiveDecoder::load(&mem)?;
+		for file in dec {
+			let file = file?;
+			println!("{} ({} bytes)", file.name(), file.data().len());
+		}
+		Ok(())
+	}
+}
+
+fn main() -> ExitCode {
+	let args = Args::parse();
+	let res = match args.command {
+		Command::List(p) => p.run(),
+		Command::Pack(p) => p.run(),
+		Command::Unpack(p) => p.run(),
+	};
+
+	match res {
+		Ok(()) => ExitCode::SUCCESS,
+		Err(e) => {
+			eprintln!("Error: {}", e);
+			ExitCode::FAILURE
+		}
+	}
+}
